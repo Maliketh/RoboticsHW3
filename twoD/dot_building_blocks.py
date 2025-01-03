59aaa7531eccff4a63809247833fed5356b783c9
Danyil Vaida <danilavajda@gmail.com>
2025-01-03 20:39:19 +0200

    Minor fix

--- twoD/dot_building_blocks.py
+++ twoD/dot_building_blocks.py
@@ -13,40 +13,22 @@ class DotBuildingBlocks2D(object):
         self.vis_dist = 60.0
 
     def compute_distance(self, prev_config, next_config):
-        """
-        Compute the Euclidean distance between two configurations.
-        @param prev_config Previous configuration.
-        @param next_config Next configuration.
-        """
         return np.linalg.norm(np.array(next_config) - np.array(prev_config))
 
     def sample_random_config(self, goal_prob, goal):
-        """
-        Samples a random configuration in the environment with a certain probability of returning the goal.
-        Ensures the sampled configuration is collision-free.
+        if np.random.rand() < goal_prob:
+            return goal
 
-        Args:
-            goal_prob (float): Probability of sampling the goal configuration.
-            goal (tuple): The goal configuration as (x, y).
-
-        Returns:
-            tuple: A randomly sampled configuration (x, y) that is collision-free.
-        """
         while True:
-            if np.random.rand() < goal_prob:
-                # With probability `goal_prob`, return the goal configuration
-                config = goal
-            else:
-                # Sample a random configuration within the bounds of the environment
-                x_min, x_max = self.x_bounds
-                y_min, y_max = self.y_bounds
+            # Sample a random configuration within the bounds of the environment
+            x_min, x_max = self.env.xlimit[0], self.env.xlimit[1]
+            y_min, y_max = self.env.ylimit[0], self.env.ylimit[1]
 
-                x_random = np.random.uniform(x_min, x_max)
-                y_random = np.random.uniform(y_min, y_max)
-                config = (x_random, y_random)
+            x_random = np.random.uniform(x_min, x_max)
+            y_random = np.random.uniform(y_min, y_max)
+            config = (x_random, y_random)
 
-            # Check if the configuration is collision-free
-            if self.is_collision_free(config):
+            if self.config_validity_checker(config):
                 return config
 
     def config_validity_checker(self, state):
@@ -54,5 +36,3 @@ class DotBuildingBlocks2D(object):
 
     def edge_validity_checker(self, state1, state2):
         return self.env.edge_validity_checker(state1, state2)
-
-
