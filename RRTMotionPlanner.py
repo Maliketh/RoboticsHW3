ea1104bc90b0a69e7f622c944f7e982996a1c1a9
Danyil Vaida <danilavajda@gmail.com>
2025-01-03 20:38:50 +0200

    RRT fixed

--- RRTMotionPlanner.py
+++ RRTMotionPlanner.py
@@ -3,10 +3,9 @@ from RRTTree import RRTTree
 import time
 
 class RRTMotionPlanner(object):
-    def __init__(self, bb, dbb, ext_mode, goal_prob, step_size, start, goal):
+    def __init__(self, bb, ext_mode, goal_prob, step_size, start, goal):
         # set environment and search tree
         self.bb = bb
-        self.dbb = dbb
         self.tree = RRTTree(self.bb)
         self.start = start
         self.goal = goal
@@ -23,26 +22,29 @@ class RRTMotionPlanner(object):
         # Tree sampling
         self.tree.add_vertex(self.start)
         while not self.tree.is_goal_exists(self.goal):
-            rand_config = self.dbb.sample_random_config(self.goal_prob, self.goal)  # assuming returns a valid config
+            rand_config = self.sample_random_config(self.goal_prob, self.goal)  # assuming returns a valid config
             near_config_id, near_config = self.tree.get_nearest_config(rand_config)
             new_config = self.extend(near_config, rand_config)
 
-            if self.dbb.edge_validity_checker(new_config, near_config):
+            if self.bb.edge_validity_checker(new_config, near_config):
                 vid = self.tree.add_vertex(new_config)
                 cost = self.bb.compute_distance(new_config, near_config)
                 self.tree.add_edge(near_config_id, vid, cost)
-
+        print(f"Tree is generated and contains: {len(self.tree.vertices)} vertices")
         # Plan computation
         plan = [self.goal]
-        while self.start not in plan:
-            child = plan[0]
-            child_idx = self.tree.get_idx_for_config(child)
-            parent_idx = self.tree.edges[child_idx]
-            parent = self.tree.vertices[parent_idx].config
-
-            plan.insert(0, parent)
-
-        return np.array(plan)
+        try:
+            while not np.allclose(plan[0], self.start, atol=1e-6):
+                child = plan[0]
+                child_idx = self.tree.get_idx_for_config(child)
+                parent_idx = self.tree.edges[child_idx]
+                parent = self.tree.vertices[parent_idx].config
+                plan.insert(0, parent)
+            print(np.array(plan).shape)
+            return np.array(plan)
+        except KeyError as e:
+            print(f"No path found between {self.start} and {self.goal}")
+            return None
 
     def compute_cost(self, plan):
         '''
@@ -74,3 +76,17 @@ class RRTMotionPlanner(object):
             new_config = direction * self.step_size + near_config
 
         return new_config
+
+    def sample_random_config(self, goal_prob, goal):
+        if np.random.rand() < goal_prob:
+            return goal
+
+        while True:
+            if len(self.goal) == 4:
+                # REGULAR CASE
+                config = np.random.uniform(low=-np.pi, high=np.pi, size=(4,))
+            else:
+                # DOT_ENV CASE
+                config = self.bb.sample_random_config(goal_prob, self.goal)
+            if self.bb.config_validity_checker(config):
+                return config
