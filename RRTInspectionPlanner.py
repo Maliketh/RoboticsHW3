09c4453a54d23bb939925303a98f6915acf34173
Danyil Vaida <danilavajda@gmail.com>
2025-01-08 13:08:46 +0200

    config validity check added

--- RRTInspectionPlanner.py
+++ RRTInspectionPlanner.py
@@ -25,17 +25,21 @@ class RRTInspectionPlanner(object):
         '''
         # Tree sampling
         print(f"---------------------------Tree sampling started! Goal coverage: {self.coverage}")
+        iter = 0
         self.tree.add_vertex(self.start, self.bb.get_inspected_points(self.start))
         while self.coverage > self.tree.max_coverage:
+            iter += 1
             rand_config = self.sample_random_config(self.goal_prob)
             near_config_id, near_config = self.tree.get_nearest_config(rand_config)
             new_config = self.extend(near_config, rand_config)
 
             if self.bb.edge_validity_checker(new_config, near_config):
                 v = len(self.tree.vertices)
-                if v % 100 == 0:
+
+                if v % 500 == 0:
                     print(f"Num vertices: {v}")
                     print(f"Current coverage: {self.tree.max_coverage}")
+                    print(f"Num of iterations: {iter}")
 
                 inspected_pts = self.bb.compute_union_of_points(self.tree.vertices[near_config_id].inspected_points,
                                                                 self.bb.get_inspected_points(new_config))
@@ -63,17 +67,21 @@ class RRTInspectionPlanner(object):
 
     def sample_random_config(self, goal_prob):
         if np.random.rand() < goal_prob:
-            count = 0
             best_inspected_pts = self.tree.vertices[self.tree.max_coverage_id].inspected_points
-            while count < 100:
+            count = 0
+            while count < 150:
                 random_config = np.random.uniform(low=-np.pi, high=np.pi, size=(4,))
-                pts_seen = self.bb.get_inspected_points(random_config)
-                if np.setdiff1d(pts_seen, best_inspected_pts).size != 0:
-                    continue
+                if self.bb.config_validity_checker(random_config):
+                    pts_seen = self.bb.get_inspected_points(random_config)
+                    if np.setdiff1d(pts_seen, best_inspected_pts).size != 0:
+                        return random_config
                 count += 1
-            return random_config
-        else:
-            return np.random.uniform(low=-np.pi, high=np.pi, size=(4,))
+
+        while True:
+            random_config = np.random.uniform(low=-np.pi, high=np.pi, size=(4,))
+            if self.bb.config_validity_checker(random_config):
+                return random_config
+
 
     def compute_cost(self, plan):
         '''
